9eaf6b6dda06723878711208850ddf980cda2406
Thiago Figueiredo <th.leonardo99@gmail.com>
2022-04-07 17:11:53 -0300

    segunda entraga concert final

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<KanbanContext>(options =>
+    options.UseNpgsql("Defautconnection"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddEntityFrameworkNpgsql()
-    .AddDbContext<KanbanContext>(options =>
-    options.UseNpgsql("Host=tuffi.db.elephantsql.com;Port=5432;Database=rxdjczzp;User Id=rxdjczzp;Password=RMammICvle6fYW2eF6RzFR7cI80CglBd;"));
 
 var app = builder.Build();
 
